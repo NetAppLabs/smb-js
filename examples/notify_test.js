f2ac333987e1737de9582ff4489a07f54d9f3296
Tryggvi Larusson <tryggvi.larusson@netapp.com>
2024-12-05 15:10:06 +0000

    adding examples

---
+++ examples/notify_test.js
@@ -0,0 +1,36 @@
+
+
+import { SmbDirectoryHandle } from '@netapplabs/smb-js';
+
+try {
+   let SMB_URL = "smb://guest@127.0.0.1:10445/smbtest"
+   let smbHandle = new SmbDirectoryHandle(SMB_URL);
+   console.log("smbHandle: ", smbHandle);
+   // watch directory 'test' with callback function provided
+   smbHandle.watch(async (watchEvent) => {
+      console.log("watchEvent returned:", watchEvent)
+      let filePath = watchEvent.path;
+      let fileAction = watchEvent.action;
+      console.log(" -- FilePath being noted:", filePath)
+      console.log(" -- File Action being noted:", fileAction)
+
+      if (!filePath.startsWith("eventLog.csv")) {
+         try {
+            // writing event back to filesystem in eventLog file
+            let eventLogFileName = "eventLog.csv";
+            let smbHandle2 = new SmbDirectoryHandle(SMB_URL);
+
+            let fh = await smbHandle2.getFileHandle(eventLogFileName, { create: true });
+            let fileWriter = await fh.createWritable({ keepExistingData: true });
+            const logString = watchEvent.path + "," + fileAction + "\n";
+            await fileWriter.write(logString);
+            await fileWriter.close();
+         } catch (err) {
+            console.log("Error writing to log", err);
+         }
+      }
+   })
+
+} catch (err) {
+   console.log("err: ", err);
+}
