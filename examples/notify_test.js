fcc5f603f0273b322eb1f4c3fee355eec6f3f2e7
Tryggvi Larusson <tryggvi.larusson@netapp.com>
2024-12-09 16:08:20 +0000

    adding example and temporarily disabling arm64 linux

--- examples/notify_test.js
+++ examples/notify_test.js
@@ -1,36 +1,30 @@
+import { SmbDirectoryHandle } from '@netapplabs/smb-js'
 
+let SMB_URL = "smb://guest@127.0.0.1:10445/smbtest/test"
+console.log(` -- Starting watch on directory ${SMB_URL}`);
+let smbHandle = new SmbDirectoryHandle(SMB_URL);
 
-import { SmbDirectoryHandle } from '@netapplabs/smb-js';
+// watch directory 'test' with callback function provided
+smbHandle.watch(async (watchEvent) => {
+   console.log("watchEvent returned:", watchEvent)
+   let filePath = watchEvent.path;
+   let fileAction = watchEvent.action;
+   console.log(" -- FilePath being noted:", filePath)
+   console.log(" -- File Action being noted:", fileAction)
 
-try {
-   let SMB_URL = "smb://guest@127.0.0.1:10445/smbtest"
-   let smbHandle = new SmbDirectoryHandle(SMB_URL);
-   console.log("smbHandle: ", smbHandle);
-   // watch directory 'test' with callback function provided
-   smbHandle.watch(async (watchEvent) => {
-      console.log("watchEvent returned:", watchEvent)
-      let filePath = watchEvent.path;
-      let fileAction = watchEvent.action;
-      console.log(" -- FilePath being noted:", filePath)
-      console.log(" -- File Action being noted:", fileAction)
+   if (!filePath.startsWith("eventLog.csv")) {
+      try {
+         // writing event back to filesystem in eventLog file
+         let eventLogFileName = "eventLog.csv";
+         let smbHandle2 = new SmbDirectoryHandle(SMB_URL);
 
-      if (!filePath.startsWith("eventLog.csv")) {
-         try {
-            // writing event back to filesystem in eventLog file
-            let eventLogFileName = "eventLog.csv";
-            let smbHandle2 = new SmbDirectoryHandle(SMB_URL);
-
-            let fh = await smbHandle2.getFileHandle(eventLogFileName, { create: true });
-            let fileWriter = await fh.createWritable({ keepExistingData: true });
-            const logString = watchEvent.path + "," + fileAction + "\n";
-            await fileWriter.write(logString);
-            await fileWriter.close();
-         } catch (err) {
-            console.log("Error writing to log", err);
-         }
+         let fh = await smbHandle2.getFileHandle(eventLogFileName, { create: true });
+         let fileWriter = await fh.createWritable({ keepExistingData: true });
+         const logString = watchEvent.path + "," + fileAction + "\n";
+         await fileWriter.write(logString);
+         await fileWriter.close();
+      } catch (err) {
+         console.log("Error writing to log", err);
       }
-   })
-
-} catch (err) {
-   console.log("err: ", err);
-}
+   }
+})
